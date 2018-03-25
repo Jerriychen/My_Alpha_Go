ecd2e01408af93fbfeb6cba73df0f06919952e0e
chenzzh <chenzzh01@gmail.com>
2018-03-25 16:28:33 +0800

    Merge branch 'second'
    Signed-off-by: chenzzh <chenzzh01@gmail.com>
    
    # Conflicts:
    #	templates/hello.html

--- static/images/large/board.js
+++ static/images/large/board.js
@@ -13,7 +13,7 @@ JGO.BOARD.large = {
     },
 
     // Margins around the board, both on normal edges and clipped ones
-    margin: {normal: 60, clipped: 40},
+    margin: {normal: 30, clipped: 30},
 
     // Shadow color, blur and offset
     boardShadow: {color: '#ffe0a8', blur: 30, offX: 5, offY: 5},
@@ -22,23 +22,23 @@ JGO.BOARD.large = {
     border: {color: 'rgba(255, 255, 255, 0.3)', lineWidth: 2},
 
     // Amount of "extra wood" around the grid where stones lie
-    padding: {normal: 20, clipped: 10},
+    padding: {normal: 10, clipped: 0},
 
     // Grid color and size, line widths
-    grid: {color: '#202020', x: 80, y: 80, smooth: 0.0,
-        borderWidth: 1.5, lineWidth: 1.2},
+    grid: {color: '#202020', x: 40, y: 40, smooth: 0.0,
+        borderWidth: 1.8, lineWidth: 1.5},
 
     // Star point radiuste
-    stars: {radius: 10},
+    stars: {radius: 5},
 
     // Coordinate color and font
     coordinates: {color: '#808080', font: 'normal 18px sanf-serif'},
 
     // Stone radius  and alpha for semi-transparent stones
-    stone: {radius: 30, dimAlpha:0.6},
+    stone: {radius: 20, dimAlpha:0.5},
 
     // Shadow offset from center
-    shadow: {xOff: -2, yOff: 2},
+    shadow: {xOff: 0, yOff: 0},
 
     // Mark base size and line width, color and font settings
     mark: {lineWidth: 1.5, blackColor: 'white', whiteColor: 'black',
@@ -48,7 +48,7 @@ JGO.BOARD.large = {
 JGO.BOARD.largeWalnut = JGO.util.extend(JGO.util.extend({}, JGO.BOARD.large), {
     textures: {board: '/static/images/large/walnut.jpg', shadow: '/static/images/large/shadow_dark.png'},
     boardShadow: {color: '#e2baa0'},
-    grid: {color: '#101010', borderWidth: 1.8, lineWidth: 1.5}
+    grid: {color: '#101010', borderWidth:0.5, lineWidth: 0.5}
 });
 
 JGO.BOARD.largeBW = JGO.util.extend(JGO.util.extend({}, JGO.BOARD.large), {
