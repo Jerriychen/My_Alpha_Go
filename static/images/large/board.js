4b0bcb56ee71e81cc2ef88c9931bc55a4c3b5363
chenzzh <chenzzh01@gmail.com>
2018-03-29 19:10:07 +0800

    second chang something, while maybe dont chang anything
    Signed-off-by: chenzzh <chenzzh01@gmail.com>

--- static/images/large/board.js
+++ static/images/large/board.js
@@ -16,17 +16,17 @@ JGO.BOARD.large = {
     margin: {normal: 25, clipped: 25},
 
     // Shadow color, blur and offset
-    boardShadow: {color: '#ffe0a8', blur: 20, offX: 0, offY: 0},   //blur 设置棋盘阴影的
+    boardShadow: {color: '#ffe0a8', blur: 30, offX: 5, offY: 5},   //blur 设置棋盘阴影的
 
     // Lighter border around the board makes it more photorealistic
-    border: {color: 'rgba(255, 255, 255, 0)', lineWidth: 100},
+    border: {color: 'rgba(255, 255, 255, 0)', lineWidth: 2},
 
     // Amount of "extra wood" around the grid where stones lie
     padding: {normal: 5, clipped: 10},
 
     // Grid color and size, line widths
     grid: {color: '#202020', x: 21, y: 21, smooth: 0.0,                     //设置棋盘的大小
-        borderWidth: 1.5, lineWidth: 0.5},
+        borderWidth: 1.5, lineWidth: 1.2},
 
     // Star point radiuste
     stars: {radius: 3},                        //设置棋盘上五个黑点的大小
@@ -35,10 +35,10 @@ JGO.BOARD.large = {
     coordinates: {color: '#808080', font: 'normal 10px sanf-serif'},
 
     // Stone radius  and alpha for semi-transparent stones
-    stone: {radius: 8},                       //棋盘上棋子中间那个白圈子的大小
+    stone: {radius: 8, dimAlpha:0.6},                       //棋盘上棋子中间那个白圈子的大小
 
     // Shadow offset from center
-    shadow: {xOff: 0, yOff: 0},
+    shadow: {xOff: -2, yOff:2},
 
     // Mark base size and line width, color and font settings
     mark: {lineWidth: 1.5, blackColor: 'white', whiteColor: 'black',
